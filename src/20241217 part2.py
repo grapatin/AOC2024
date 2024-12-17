87ab97ac3982fe4330850dd153db86d03712d104
grapatin <per.grapatin@gmail.com>
2024-12-17 22:07:08 +0100

    Part 2 solved

--- src/20241217 part2.py
+++ src/20241217 part2.py
@@ -44,7 +44,6 @@ def solve(input_string: str) -> int:
     #Get each int in program and pass into a list
     program = list(map(int, program_string.split(",")))
     len_input_program = len(program)
-    best_result = 0
 
     # We should try to solve this by getting same output as the program
     # each octet in A creates one output
@@ -54,78 +53,91 @@ def solve(input_string: str) -> int:
     # the number of octets in A will be same as number of steps in the program
     # we can try to solve this by brute force
 
-    result = 0
+    #result = 8**len_input_program
+    results = []
+    possible_values = []
+    possible_values.append(0)
+
+    lowest_value = 8**(len_input_program + 1)
     output_solved_so_far = 0
     for octet_number in range(len_input_program):
+        #lets try to solve with highest octet first
+        octet_number = len_input_program - octet_number - 1
         print("Octet number:", octet_number)
-        
-        for k in range(8):
-            start_value = k*(8**octet_number) + result
-            registers_dict["A"] = k*(8**octet_number) + result
-            #registers_dict["A"] = int("0o345300", 8)
-            #registers_dict["A"] = int("0o345000", 8)
+        results = possible_values.copy()    
 
-            registers_dict["B"] = 0
-            registers_dict["C"] = 0
-            # Execute the program
-            ip = 0
-            output = ''
-            output_list = []
-            len_input_program = len(program)
+        for result_input in results:
+            for k in range(8):
+                start_value = k*(8**octet_number) + result_input
+                registers_dict["A"] = start_value
+                #registers_dict["A"] = int("0o345300", 8)
+                #registers_dict["A"] = int("0o4532316267433310", 8)
 
-            while ip < len(program):
-                instruction = program[ip]
-                combo_operand = combo_check(program[ip + 1])
-                literal_operand = program[ip + 1]
-                if instruction == 0:
-                    # adv division
-                    numerator = registers_dict["A"]
-                    denominator = 2 ** combo_operand
-                    #result = numerator / denominator but truncated to int
-                    registers_dict["A"] =  numerator // denominator
-                elif instruction == 1:
-                    # bxl bitwise xor register B and literal operand
-                    registers_dict["B"] ^= literal_operand 
-                elif instruction == 2:
-                    #bst combo operand modulo 8 store in register B
-                    registers_dict["B"] = combo_operand % 8
-                elif instruction == 3:
-                    # jnx instructior
-                    if registers_dict["A"] == 0:
-                        pass
-                    else:
-                        ip = literal_operand
-                        continue
-                    # leave ip to increase as normal
-                elif instruction == 4:
-                    # bxc bitwise xor register B and register C
-                    registers_dict["B"] = registers_dict["B"] ^ registers_dict["C"]
-                elif instruction == 5:
-                    # out, combo operand modulo 8
-                    output += str(combo_operand % 8) + ","
-                    output_list.append(combo_operand % 8)
-                elif instruction == 6:
-                    # bdv 
-                    numerator = registers_dict["A"]
-                    denominator =  2 ** combo_operand
-                    #result = numerator / denominator but truncated to int
-                    registers_dict["B"] = numerator // denominator
-                elif instruction == 7:
-                    # cdv 
-                    numerator = registers_dict["A"]
-                    denominator =  2 ** combo_operand
-                    #result = numerator / denominator but truncated to int
-                    registers_dict["C"] = numerator // denominator
-                ip += 2
+                registers_dict["B"] = 0
+                registers_dict["C"] = 0
+                # Execute the program
+                ip = 0
+                output = ''
+                output_list = []
 
-            if output_solved_so_far < len(output_list) and all(output_list[i] == program[i] for i in range(output_solved_so_far + 1)):
-                print("Found a match for octet number:", output_solved_so_far, "with start value of A:", oct(start_value))
-                print("Output:", output_list)
-                print("Program:", program)
-                result = k*(8**octet_number) + result
-                output_solved_so_far += 1
-                break
-    return result
+                while ip < len(program):
+                    instruction = program[ip]
+                    combo_operand = combo_check(program[ip + 1])
+                    literal_operand = program[ip + 1]
+                    if instruction == 0:
+                        # adv division
+                        numerator = registers_dict["A"]
+                        denominator = 2 ** combo_operand
+                        #result = numerator / denominator but truncated to int
+                        registers_dict["A"] =  numerator // denominator
+                    elif instruction == 1:
+                        # bxl bitwise xor register B and literal operand
+                        registers_dict["B"] ^= literal_operand 
+                    elif instruction == 2:
+                        #bst combo operand modulo 8 store in register B
+                        registers_dict["B"] = combo_operand % 8
+                    elif instruction == 3:
+                        # jnx instructior
+                        if registers_dict["A"] == 0:
+                            pass
+                        else:
+                            ip = literal_operand
+                            continue
+                        # leave ip to increase as normal
+                    elif instruction == 4:
+                        # bxc bitwise xor register B and register C
+                        registers_dict["B"] = registers_dict["B"] ^ registers_dict["C"]
+                    elif instruction == 5:
+                        # out, combo operand modulo 8
+                        output += str(combo_operand % 8) + ","
+                        output_list.append(combo_operand % 8)
+                    elif instruction == 6:
+                        # bdv 
+                        numerator = registers_dict["A"]
+                        denominator =  2 ** combo_operand
+                        #result = numerator / denominator but truncated to int
+                        registers_dict["B"] = numerator // denominator
+                    elif instruction == 7:
+                        # cdv 
+                        numerator = registers_dict["A"]
+                        denominator =  2 ** combo_operand
+                        #result = numerator / denominator but truncated to int
+                        registers_dict["C"] = numerator // denominator
+                    ip += 2
+                #check if last field in output_list is same as program[-1]
+                if len(output_list) == len_input_program:
+                    if output_list[len_input_program - output_solved_so_far - 1] == program[len_input_program - output_solved_so_far - 1]: 
+                        #print("Output solved:", output_list[len_input_program - output_solved_so_far - 1], "output_solved_so_far:", output_solved_so_far, "current_value:", oct(start_value))   
+                        possible_values.append(start_value)
+                        #print(output_list)
+                        #print(program)
+                        #check if all values match
+                        if output_list == program:
+                            if start_value < lowest_value:
+                                lowest_value = start_value
+                                print("Lowest value so far:", oct(lowest_value))
+        output_solved_so_far += 1
+    return lowest_value
 
 # Test the example
 result = solve(example_input)
